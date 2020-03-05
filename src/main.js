84b330d2c473e8050dbbab87ce5437c9da6b9ce7
Jackie <16275507@qq.com>
2020-03-05 21:48:08 +0800

    update directory structure, dependencies version and permission control

---
+++ src/main.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Element from 'element-ui'
+
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+import '@/assets/styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
+import router from './router'
+
+import './auth' // permission control
+import * as filters from './filters' // global filters
+import './vendors/font-awesome-icon' // Font Awesome Icon
+
+Vue.use(Element)
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJS for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJS will be used in the production environment,
+ * please remove it before going online ! ! !
+ */
+// if (process.env.NODE_ENV === 'production') {
+//   const { mockXHR } = require('../mock')
+//   mockXHR()
+// }
+
+// register global utility filters
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
+// register an event hub
+Vue.prototype.$eventHub = Vue.prototype.$eventHub || new Vue()
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
