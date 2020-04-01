a885f68e55d31b06598d4df0ecec6ec528379ad6
Jackie <16275507@qq.com>
2020-04-01 20:30:36 +0800

    update SvgIcon

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -121,3 +121,41 @@ export function isScrolledToBottom (element) {
   const scrollHeight = element && element.scrollHeight || 0
   return getFinalStyle(element, 'overflow-y') === 'hidden' || scrollHeight <= clientHeight || Math.ceil(scrollTop) + clientHeight >= scrollHeight
 }
+
+/**
+ * @description determine whether the val is empty
+ * @param { * } val
+ * @return { Boolean }
+ * @author Jackie
+ * @date 2020-04-01 20:30
+ */
+export const isEmpty = (val) => {
+  // null or undefined
+  if (val == null) return true
+
+  if (typeof val === 'boolean') return false
+
+  if (typeof val === 'number') return !val
+
+  if (val instanceof Error) return val.message === ''
+
+  switch (Object.prototype.toString.call(val)) {
+    // String or Array
+    case '[object String]':
+    case '[object Array]':
+      return !val.length
+
+    // Map or Set or File
+    case '[object File]':
+    case '[object Map]':
+    case '[object Set]': {
+      return !val.size
+    }
+    // Plain Object
+    case '[object Object]': {
+      return !Object.keys(val).length
+    }
+  }
+
+  return false
+}
