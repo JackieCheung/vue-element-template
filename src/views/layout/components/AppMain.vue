edc02c212ffead27ddb60f6896b1558d1361a9c4
Jackie Cheung <16275507@qq.com>
2020-05-26 17:34:49 +0800

    fix: keep alive bug

--- src/views/layout/components/AppMain.vue
+++ src/views/layout/components/AppMain.vue
@@ -17,7 +17,7 @@
     name: 'AppMain',
     computed: {
       cachedViews () {
-        return this.$store.state.routerView.cachedViews
+        return ['ParentView', ...this.$store.state.routerView.cachedViews]
       },
       key () {
         return this.$route.path
