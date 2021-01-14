8025cbd8a176f71922fc7f135e6e8fd84e5b2617
Jackie Cheung <16275507@qq.com>
2021-01-14 23:27:45 +0800

    feat: add VUE_APP_PROXY_TARGET

--- vue.config.js
+++ vue.config.js
@@ -41,7 +41,7 @@ module.exports = {
       // change xxx-api/login => mock/login
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://127.0.0.1:${port}/mock`,
+        target: process.env.VUE_APP_PROXY_TARGET,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
